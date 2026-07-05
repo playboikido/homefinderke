9b2d7daaf3251f4f22644e02dd2756e8d5f9f673
david muigai <davidmuigaiw@gmail.com>
2026-07-05 21:32:38 +0300

    Fix settings module reference in create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homefinderke.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 from django.contrib.auth.models import User
 
