7b12df51ecdf21db48c2319604b13a6b26af0b22
playboikido <davidmuigaiw@gmail.com>
2026-07-07 16:03:54 +0300

    Increase Gunicorn timeout to 60 seconds

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
