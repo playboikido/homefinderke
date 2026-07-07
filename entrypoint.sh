313368fd5cd1111d59cd10563c90bcdf7e51dd10
playboikido <davidmuigaiw@gmail.com>
2026-07-07 16:10:57 +0300

    Reduce Gunicorn workers from 3 to 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 1 --timeout 60
