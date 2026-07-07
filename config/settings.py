79223836cf78970b8709b0659fe28242180bca98
playboikido <davidmuigaiw@gmail.com>
2026-07-07 16:06:20 +0300

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', '.northflank.app',]
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', '.code.run',]
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'https://*.code.run']
 
 # Application definition
 
