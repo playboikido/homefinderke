7a01fbe4b68845a7350eead2eb5a72df5c8f2c7e
playboikido <davidmuigaiw@gmail.com>
2026-07-07 16:22:01 +0300

    Fix formatting of ALLOWED_HOSTS in settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', '.code.run',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', '.code.run']
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'https://*.code.run']
 
 # Application definition
