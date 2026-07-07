1333897dd549f8e87cee7916841b2084cae96a30
playboikido <davidmuigaiw@gmail.com>
2026-07-07 16:52:49 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 1 --timeout 60
