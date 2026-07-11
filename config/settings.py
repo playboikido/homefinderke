03aed0b4ce096146118b87c7c36366c9652dc310
david muigai <davidmuigaiw@gmail.com>
2026-07-11 11:46:26 +0300

    Fix invisible chat input text and balance input/button sizing

--- config/settings.py
+++ config/settings.py
@@ -207,7 +207,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = 'homefinder.ke.help@gmail.com'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
