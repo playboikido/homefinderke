cb387f015cde4a101d26b5113cd22753d40210b3
david muigai <davidmuigaiw@gmail.com>
2026-07-20 14:51:17 +0300

    Switch to Gmail SMTP with app password since Postmark blocks public-domain sender addresses

--- config/settings.py
+++ config/settings.py
@@ -228,13 +228,13 @@ LOGOUT_REDIRECT_URL = 'home'
 MFA_SUPPORTED_TYPES = ['totp', 'recovery_codes']
 MFA_TOTP_ISSUER = 'HomeFinder KE'
 
-EMAIL_BACKEND = 'postmarker.django.EmailBackend'
-EMAIL_TIMEOUT = 10  # never let a hung email call risk crashing the worker
-POSTMARK = {
-    'TOKEN': os.environ.get('POSTMARK_SERVER_TOKEN'),
-    'TEST_MODE': False,
-    'VERBOSITY': 0,
-}
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_TIMEOUT = 10
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'homefinder.ke.help@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
 
 DEFAULT_FROM_EMAIL = "homefinder.ke.help@gmail.com"
 
