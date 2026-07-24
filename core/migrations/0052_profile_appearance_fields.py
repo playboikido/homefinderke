39185320e2f88c596b8ef1aa151cf33d8de0a33e
david muigai <davidmuigaiw@gmail.com>
2026-07-24 14:44:56 +0300

    Fix migration dependency chain: point 0052 to correct NotificationPreference migration

--- core/migrations/0052_profile_appearance_fields.py
+++ core/migrations/0052_profile_appearance_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0051_notificationpreference'),
+        ('core', '0051_profile_compact_mode_profile_hide_email_and_more'),
     ]
 
     operations = [
