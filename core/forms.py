541cc3d7de93f15b006e823945a0a1eb7a033e3e
david muigai <davidmuigaiw@gmail.com>
2026-08-09 01:01:15 +0300

    Remove dead/unreachable duplicate auth flow at /accounts/custom/

--- core/forms.py
+++ core/forms.py
@@ -263,28 +263,6 @@ class SettingsProfileForm(forms.ModelForm):
         }
 
 
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django_recaptcha.fields import ReCaptchaField
-from django_recaptcha.widgets import ReCaptchaV2Checkbox
-
-
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    agree_to_terms = forms.BooleanField(
-        required=True,
-        error_messages={'required': 'You must agree to the Terms of Service to create an account.'}
-    )
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-class UserLoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
 
 
 from django_recaptcha.fields import ReCaptchaField
